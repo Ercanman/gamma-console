9c1f8732143cff20d75620a8d4472b61812f46fe
Lykka <53619033+Lykka@users.noreply.github.com>
2022-09-09 12:29:42 +0200

    Member client
    get member by id
    get team member by team name

---
+++ gamma-console/Client/MemberClient.cs
@@ -0,0 +1,58 @@
+﻿using gamma_console.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace gamma_console.Client
+{
+    internal class MemberClient
+    {
+        
+        //public HttpClient GetMemberClient()
+        //{
+        //    return null;
+        //}
+
+        public List<Member> getTeamMember(string teamName)
+
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://app-lab-codingdojo-alpha.azurewebsites.net")
+            };
+            HttpResponseMessage response = client.GetAsync("Member/GetMembersByTeam/" + teamName).Result;
+            response.EnsureSuccessStatusCode();
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+           // Console.WriteLine(responseBody);
+
+            //resonseBody innehåller alla personer
+            List<Member> teamMembers = JsonConvert.DeserializeObject<List<Member>>(responseBody);
+
+            return teamMembers;
+        }
+
+        public MemberWithFavoriteMovies GetMember(Guid id)
+        {
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://app-lab-codingdojo-alpha.azurewebsites.net")
+            };
+            HttpResponseMessage response = client.GetAsync("Member/GetMemberById/" + id).Result;
+            response.EnsureSuccessStatusCode();
+            string responseBody = response.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine(responseBody);
+
+            //resonseBody innehåller alla personer
+            MemberWithFavoriteMovies? memberWithFav = JsonConvert.DeserializeObject<MemberWithFavoriteMovies>(responseBody);
+
+            return memberWithFav;
+        }
+    }
+}
--- gamma-console/Program.cs
+++ gamma-console/Program.cs
@@ -1,8 +1,13 @@
-﻿internal class Program
+﻿using gamma_console.Client;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.Write("Write teamname: ");
+        MemberClient mc = new MemberClient();
+        var tmp=mc.GetMember(new Guid("79492e57-2638-46ab-ba6e-08da76c71246"));
+        mc.getTeamMember("gamma");
         
 
     }
